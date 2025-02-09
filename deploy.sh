bf03629a0572c7d90f1d4b0970b0b66fd2a65260
srxrt <str9703@gmail.com>
2025-02-09 16:56:56 +0900

    feat: integrate deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#PRODUCTION
+git reset --hard
+git checkout master
+git pull origin master
+
+npm i yarn -g
+yarn global add serve
+yarn
+yarn run build
+pm2 start "yarn run start:prod" --name=BURAK-REACT
+
+#DEVELOPMENT
+# git reset --hard
+# git checkout develop
+# git pull origin develop
+#
+# npm i
+# pm2 start "npm run start:dev" --name=KING-DEV

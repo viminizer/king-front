30f62969229f25f0bf1d533d4fdc2ceec52e5e8b
srxrt <str9703@gmail.com>
2025-02-14 02:39:03 +0900

    feat: added commit.sh

---
+++ commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git checkout develop
+git add .
+git status
+
+# Prompt the user for the commit message
+echo "Enter commit message: "
+read COMMIT_MESSAGE
+
+# Commit the changes with the provided message
+git commit -m "$COMMIT_MESSAGE"
+git push origin develop
+
+echo "Switching to master branch..."
+git checkout master
+git merge develop
+git push origin master
+
+echo "Switching to develop branch..."
+git checkout develop
+echo "Changes have been committed with message: '$COMMIT_MESSAGE'"
